44b8d9ef9b702037e5c4414dc054d1249dd54c7b
arjavjain59998 <arjavjain59998@gmail.com>
2019-04-15 13:10:56 +0530

    question 1

--- MysteryCoding/day1/views.py
+++ MysteryCoding/day1/views.py
@@ -20,13 +20,6 @@ def email(request):
 
 @login_required
 def story1(request):
-    # form = Ans1()
-    if request.method == 'POST':
-        form = Ans1(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = Ans1()
-    return render(request, 'day1/story_before_q1.html', {'ans': form})
+
+    return render(request, 'day1/story_before_q1.html')
 
